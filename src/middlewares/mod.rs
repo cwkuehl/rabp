8f1f6d765e9ca7a4af9fe429bc569ab7c1313539
wolfgang <wolfgang@cwkuehl.de>
2023-10-19 15:17:29 +0200

    Simple middleware created.

--- src/middlewares/mod.rs
+++ src/middlewares/mod.rs
@@ -2,8 +2,10 @@ mod cors;
 mod err_handlers;
 mod logger;
 mod security_headers;
+mod tls_alert;
 
 pub use self::cors::cors;
 pub use self::err_handlers::err_handlers;
 pub use self::logger::logger;
 pub use self::security_headers::security_headers;
+pub use self::tls_alert::SayHi;
