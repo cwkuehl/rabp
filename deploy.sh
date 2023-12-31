e5e2dbceeca4a05254391ffc92bc0f02a429c468
wolfgang <wolfgang@cwkuehl.de>
2023-12-31 23:11:01 +0100

    Deployment with process killing and starting anew.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 #sudo apt install libssl-dev
 # Autostart with crontab -e
+
+# Kill running instance
+pkill rabp
+
+# Update code
 git stash
 git pull --rebase
+
+# Build
 cargo build -r
 # /opt/Haushalt/rabp/.env
 cp -f ~/rust/rabp/target/release/rabp /opt/Haushalt/rabp
 cp -f ~/rust/rabp/#rabp.sh /opt/Haushalt/rabp
 #cp -f /opt/Haushalt/CSBP/cert/cert.pem /opt/Haushalt/CSBP/cert/cert.key /opt/Haushalt/rabp/cert
 # ~/hsqldb/rsbp.db
+
+# Start new instance in the background
+./#rabp.sh &
